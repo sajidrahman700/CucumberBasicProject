2e7bcf7838a89e76fcee598726225334c62fac5b
sajidrahman700 <127319733+sajidrahman700@users.noreply.github.com>
2024-02-24 11:39:38 +0600

    Hooks class

--- src/test/java/step_definitions/Hooks.java
+++ src/test/java/step_definitions/Hooks.java
@@ -1,5 +1,24 @@
 package step_definitions;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+
+import static utilities.DriverSetup.openABrowser;
+import static utilities.DriverSetup.quiteBrowser;
+
 public class Hooks {
+	
+	 public static String browserName = System.getProperty("browser", "Chrome");
+
+	    @Before
+	    public void startBrowser(){
+	        openABrowser(browserName);
+	    }
+
+	    @After
+	    public void closeBrowser(Scenario scenario){
+	        quiteBrowser(scenario);
+	    }
 
 }
